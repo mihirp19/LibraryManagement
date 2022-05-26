69ea3568f6a05714363a1a88e7a9045c95eddf73
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2022-05-26 22:39:09 +0530

    Jetlu thayu etlu thayu

---
+++ app/src/main/java/com/example/librarymanagement/StudentActivity.kt
@@ -0,0 +1,90 @@
+package com.example.librarymanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.librarymanagement.models.User
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class StudentActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavBar: BottomNavigationView
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close)
+
+
+        bottomNavBar = findViewById(R.id.bottom_nav_view)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val navView = findViewById<NavigationView>(R.id.side_nav_view)
+
+        appBarConfiguration = AppBarConfiguration.Builder(
+            setOf(
+                R.id.signOutDialogFragment,
+                R.id.studentMainFragment,
+                R.id.studentInfoFragment
+            )
+        ).setOpenableLayout(drawerLayout)
+            .build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavBar.setupWithNavController(navController)
+        navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(navView, navController)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return when (navController.currentDestination?.id) {
+                in listOf(R.id.changePasswordStudentFragment, R.id.editProfileFragmentStudent) -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    super.onOptionsItemSelected(item)
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            navController,
+            appBarConfiguration
+        ) || super.onSupportNavigateUp()
+    }
+}
